c4c4f668c3da67f41d7802ba877eba0bfd8cf845
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-02-29 23:56:32 +0300

    assignment done

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,13 @@
 
 class Person:
     # Class body goes here
+    def __init__(self):
+        pass
+    
+
+    def talk(self):
+        print("Hello World!")
 
     #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
